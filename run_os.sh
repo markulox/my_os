0915551a06d62d9edca1da3bb38b244eb7289733
Nopphon <nopphonyel@gmail.com>
2024-03-26 09:34:49 +0700

    feat: make run_os.sh a bit smarter;

--- run_os.sh
+++ run_os.sh
@@ -1,3 +1,19 @@
 #!/bin/zsh
 OS_NAME="my_os"
-qemu-system-x86_64 -drive format=raw,file=./target/x86_64-$OS_NAME/debug/bootimage-$OS_NAME.bin & ( vncviewer 127.0.0.1:5900 && killall qemu-system-x86_64 )
+# Check required binary
+echo "<I> Checking required binary..."
+if ! qemu-system-x86_64 --version &> /dev/null; then
+    echo "<X> Cannot detect qemu-system-x86_64 in your system, please install \"qemu\" first"
+fi
+if ! vncviewer -help &> /dev/null; then
+    echo "<X> Cannot detect realvnc-vnc-viewer in your system, please install \"realvnc-vnc-viewer\" frist"
+fi
+
+echo "<I> Checking bootimage existance..."
+BOOTIMAGE_PATH="./target/x86_64-"$OS_NAME"/debug/bootimage-"$OS_NAME".bin"
+if ! [ -f $BOOTIMAGE_PATH ]; then
+    echo "<I> Cannot find boot image. Building boot image..."
+    cargo bootimage
+fi
+
+qemu-system-x86_64 -drive format=raw,file=$BOOTIMAGE_PATH & ( vncviewer 127.0.0.1:5900 && killall qemu-system-x86_64 )
