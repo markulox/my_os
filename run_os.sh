c85fba7218de2783cded03957ef7ee45a6ccd8e3
Nopphon <nopphonyel@gmail.com>
2024-03-26 18:30:03 +0700

    change: run_os.sh will always rebuild image everytime that script being called;

--- run_os.sh
+++ run_os.sh
@@ -11,9 +11,10 @@ fi
 
 echo "<I> Checking bootimage existance..."
 BOOTIMAGE_PATH="./target/x86_64-"$OS_NAME"/debug/bootimage-"$OS_NAME".bin"
-if ! [ -f $BOOTIMAGE_PATH ]; then
-    echo "<I> Cannot find boot image. Building boot image..."
-    cargo bootimage
-fi
-
+# if ! [ -f $BOOTIMAGE_PATH ]; then
+#     echo "<I> Cannot find boot image. Building boot image..."
+#     cargo bootimage
+# fi
+echo "<I> Rebuilding bootimage..."
+cargo bootimage
 qemu-system-x86_64 -drive format=raw,file=$BOOTIMAGE_PATH & ( vncviewer 127.0.0.1:5900 && killall qemu-system-x86_64 )
